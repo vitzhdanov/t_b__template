78abb80cd5f26ec93281d334cca2bf88edeebd9b
vitzhdanov <vladislav.vitzhdanov@gmail.com>
2021-11-29 15:20:49 +0300

    remove env

--- data/config.py
+++ data/config.py
@@ -1,10 +1,3 @@
-from environs import Env
-
-# Теперь используем вместо библиотеки python-dotenv библиотеку environs
-env = Env()
-env.read_env()
-
-BOT_TOKEN = env.str("BOT_TOKEN")  # Забираем значение типа str
-ADMINS = env.list("ADMINS")  # Тут у нас будет список из админов
-IP = env.str("ip")  # Тоже str, но для айпи адреса хоста
+BOT_TOKEN = "2071707606:AAHNQtteqQ8_FjlEcyCeMqGgK26zwYBYsiE"  # Забираем значение типа str
+ADMINS = [495432329]  # Тут у нас будет список из админов
 
